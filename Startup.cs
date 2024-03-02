b3ffca73910866a88ec813092a6e2fa786555560
saifwsm <160519964+saifwsm@users.noreply.github.com>
2024-03-02 18:34:43 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,7 @@ namespace TodoApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = secretValue });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = mySecretValue });
             });
 
             services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("TodoList"));
